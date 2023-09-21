aaff6caae36be0350b428980eff1654a8a3f7631
Marafi Mergani <marafimergani@gmail.com>
2023-09-20 19:37:10 -0600

    added code

--- echo_server.py
+++ echo_server.py
@@ -1,33 +1,38 @@
-#!/usr/bin/env python3
 import socket
-import time
+from threading import Thread
 
-#define address & buffer size
-HOST = ""
-PORT = 8001
-BUFFER_SIZE = 1024
+BYTES_TO_READ = 4096
+Host = "127.0.0.1"
+Port = 8080
 
-def main():
+def handle_connection(conn, addr):
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(BYTES_TO_READ) # wait for request, and read it
+            if not data:
+                break
+            print(data)
+            conn.sendall(data) # send it back to the client
+
+# start single threaded echo server
+def start_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    
-        #QUESTION 3
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        
-        #bind socket to address
-        s.bind((HOST, PORT))
-        #set to listening mode
-        s.listen(2)
-        
-        #continuously listen for connections
+        s.bind((Host, Port))
+        s.listen()
         while True:
             conn, addr = s.accept()
-            print("Connected by", addr)
-            
-            #recieve data, wait a bit, then send it back
-            full_data = conn.recv(BUFFER_SIZE)
-            time.sleep(0.5)
-            conn.sendall(full_data)
-            conn.close()
+            handle_connection(conn, addr)
 
-if __name__ == "__main__":
-    main()
+# start multi threaded echo server
+def start_threaded_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((Host, Port))
+        s.listen(2)
+        while True:
+            conn, addr = s.accept()
+            thread = Thread(target=handle_connection, args=(conn, addr))
+            thread.run()
+
+# start_server()
+start_threaded_server()
